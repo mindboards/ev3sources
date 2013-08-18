ce99b035a51c918fd0901c014462481861e6f7a0
Nicolas Schodet <nico@ni.fr.eu.org>
2013-08-18 12:23:07 +0200

    Make doc generation more straightforward

--- lms2012/open_first/Build Documentation.sh
+++ lms2012/open_first/Build Documentation.sh
@@ -1,23 +1,8 @@
 #! /bin/bash
 
-cp ~/projects/lms2012/lms2012/doc/Doxyfile Doxyfile
+cd ../lms2012/doc
 doxygen Doxyfile
 cp ~/projects/lms2012/lms2012/doc/graphics/LEGO.jpg ~/projects/lms2012/lms2012/doc/html/LEGO.jpg
-cp ~/projects/lms2012/lms2012/doc/graphics/Lms2012.jpg ~/projects/lms2012/lms2012/doc/html/Lms2012.jpg
-cp ~/projects/lms2012/lms2012/doc/graphics/SystemBlock.jpg ~/projects/lms2012/lms2012/doc/html/SystemBlock.jpg
 
-cp ~/projects/lms2012/lms2012/doc/flowcharts/generalnavigation.png ~/projects/lms2012/lms2012/doc/html/
-cp ~/projects/lms2012/lms2012/doc/flowcharts/program.png ~/projects/lms2012/lms2012/doc/html/
-cp ~/projects/lms2012/lms2012/doc/flowcharts/view.png ~/projects/lms2012/lms2012/doc/html/
-cp ~/projects/lms2012/lms2012/doc/flowcharts/datalog.png ~/projects/lms2012/lms2012/doc/html/
-
-cp ~/projects/lms2012/lms2012/doc/flowcharts/motorcontrol.png ~/projects/lms2012/lms2012/doc/html/
-cp ~/projects/lms2012/lms2012/doc/flowcharts/ircontrol.png ~/projects/lms2012/lms2012/doc/html/
-cp ~/projects/lms2012/lms2012/doc/flowcharts/filenavigation.png ~/projects/lms2012/lms2012/doc/html/
-cp ~/projects/lms2012/lms2012/doc/flowcharts/buttonlight.png ~/projects/lms2012/lms2012/doc/html/
-
-rm Doxyfile
-
-cd ~/projects/lms2012/lms2012/doc/html
-firefox index.html
+firefox html/index.html
 
